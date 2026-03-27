f11fe57bb5fdd87ad3d24f1d4c7bde013c443642
William Milanez <william.milanez@outlook.com>
2026-03-26 22:10:57 -0300

    Criando o AuthLayout. Criando um layout padrão para as telas de sign-in e sign-up usando o Figma como referência. Estruturando o componente AlfLayout.tsx, importando o logo e utilizando o Outlet para renderizar as rotas. Também foi adicionado e abordado a aplicação de estilos com Tailwind CSS, como definir largura, altura e cores de fundo, além de implementar breakpoints para responsividade. O foco foi reaproveitar o design e garantir uma experiência visual consistente entre as páginas.

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router";
 
+import { AuthLayout } from "../components/AuthLayout";
 import { SignIn } from "../pages/SignIn";
 
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 }
