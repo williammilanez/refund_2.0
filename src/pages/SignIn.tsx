e56262566100b7f8edf166ae942daa50ba5cf8d2
William Milanez <william.milanez@outlook.com>
2026-03-29 15:40:05 -0300

    Criando onSubmit com os Estados. Implemantando o método onSubmit para o formulário de login. Realizando a captura das informações de e-mail e de senha, utilizando o evento de submit. Foi abordada a questão da importância de evitar o recarregamento da página com e.preventDefault(), garantindo que os dados não se percam. Além disso, foram criados estados para gerenciar os inputs e implementada a lógica para habilitar ou desabilitar o botão de envio.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/input";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    console.log(email, password);
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPassword(e.target.value)}
       />
 
-      <Input required legend="Senha" type="password" placeholder="123456" />
-
-      <Button>Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
     </form>
   );
 }
