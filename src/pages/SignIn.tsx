8fe9cd88f179406fb68057ef138b0ed903ea84ad
William Milanez <william.milanez@outlook.com>
2026-03-29 15:13:33 -0300

    Criando Componente de Button. Desenvovimento de um componente de botão em React. Inicialmente, foi criado um arquivo botão.tsx e definido suas propriedades, incluindo uma opção isLoading para indicar se o botão está em processo de carregamento. Aplicado os estilos com Tailwind CSS, como cores, transições e efeitos de hover. Também foi abordado como desabilitar o botão durante o carregamento e ajustada a opacidade. Ao final, houve a integração do botão em um formulário de login, garantindo que ele funcione corretamente.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../components/Button";
 import { Input } from "../components/input";
 
 export function SignIn() {
@@ -11,6 +12,8 @@ export function SignIn() {
       />
 
       <Input required legend="Senha" type="password" placeholder="123456" />
+
+      <Button>Entrar</Button>
     </form>
   );
 }
