35e447ae32d9ad49cbcb5f4efd9426a0040b5791
William Milanez <william.milanez@outlook.com>
2026-03-30 21:26:40 -0300

    Link para SignUp. Criação de um link que direciona para a tela de criação de conta. Inicialmente foi adicionado um botão com classes específicas para estilização, como texto em negrito, cor cinza e margens. Também foi aplicada uma animação suave para a interação do mouse. O objetivo é deixar o link visualmente atraente e funcional, mesmo que a rota ainda não exista.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -33,6 +33,13 @@ export function SignIn() {
       <Button type="submit" isLoading={isLoading}>
         Entrar
       </Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar conta
+      </a>
     </form>
   );
 }
