617bfce2f4d02b87847146cf4bbfefcb8217989d
William Milanez <william.milanez@outlook.com>
2026-04-01 22:22:50 -0300

    Criando Formulário. Construção de um formulário para solicitações de reembolso. Foram removidos elementos desnecessários e criado um cabeçalho com um título e uma descrição. Em seguida, foi feita a importação do componente de input para adicionar campos obrigatórios, como o nome da solicitação. Também foram aplicadas classes para estilizar o formulário, garantindo que ele se ajuste bem em diferentes tamanhos de tela. O objetivo é criar uma estrutura clara e funcional para o usuário.

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,3 +1,18 @@
+import { Input } from "../components/input";
+
 export function Refund() {
-  return <h1>Refund</h1>;
+  return (
+    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+      <header>
+        <h1 className="text-xl font-bold text-gray-100">
+          Solicitação de reembolso
+        </h1>
+        <p className="text-sm text-gray-200 mt-2 mb-4">
+          Dados da despesa para solicitar reembolso.
+        </p>
+      </header>
+
+      <Input required legend="Nome da solicitação" />
+    </form>
+  );
 }
