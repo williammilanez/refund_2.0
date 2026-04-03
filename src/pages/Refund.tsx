224ce7bde29f13984d580b0ac3b25e1aac5669e4
William Milanez <william.milanez@outlook.com>
2026-04-03 16:47:56 -0300

    Inputs Um Do Lado Do Outro. Continuação da criação de um formulário, focando na disposição dos elementos. Já havia sido definido o nome da solicitação e a categoria, e agora foi adicionado um novo campo para o valor. Esse novo campo, foi posicionado o input ao lado da categoria, utilizando uma div para agrupar ambos e aplicar um layout flex com espaçamento. Assim, a aplicação ficou com uma apresentação mais organizada e intuitiva para o usuário.

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -19,18 +19,23 @@ export function Refund() {
       </header>
 
       <Input required legend="Nome da solicitação" />
-      <Select
-        required
-        legend="Categoria"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        {CATEGORIES_KEYS.map((category) => (
-          <option key={category} value={category}>
-            {CATEGORIES[category].name}
-          </option>
-        ))}
-      </Select>
+
+      <div className="flex gap-4">
+        <Select
+          required
+          legend="Categoria"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {CATEGORIES_KEYS.map((category) => (
+            <option key={category} value={category}>
+              {CATEGORIES[category].name}
+            </option>
+          ))}
+        </Select>
+
+        <Input legend="Valor" />
+      </div>
     </form>
   );
 }
