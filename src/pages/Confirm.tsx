01746c6dfb8f7a22370866387c189049c3a1f62f
William Milanez <william.milanez@outlook.com>
2026-04-04 17:03:41 -0300

    Finalizando Confirm. Finalizando a interface de uma aplicação, substituindo um H1 por uma div que contém um H1 com a mensagem "solicitação enviada" e uma imagem de confirmação. Adicionei um parágrafo explicando que a solicitação será analisada e um link para retornar à página inicial. Também configurei classes para estilizar a div, o texto e o botão, garantindo uma boa responsividade e visualização. Ao final, testei como a funcionalidade de nova solicitação funciona na prática.

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,4 +1,5 @@
 import { Navigate, useLocation } from "react-router";
+import okSvg from "../assets/ok.svg";
 
 export function Confirm() {
   const location = useLocation();
@@ -7,5 +8,25 @@ export function Confirm() {
     return <Navigate to="/" />;
   }
 
-  return <h1>Confirm</h1>;
+  return (
+    <div className="bg-gray-500 lg:w-[512px] rounded-xl flex flex-col items-center p-10 gap-6">
+      <h1 className="text-2xl font-bold text-center text-green-100">
+        Solicitação enviada!
+      </h1>
+
+      <img src={okSvg} alt="Ícone de OK" className="w-28" />
+
+      <p className="text-sm text-gray-200 text-center">
+        Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o
+        setor financeiro irá entrar em contato com você.{" "}
+      </p>
+
+      <a
+        href="/"
+        className="w-full p-3 text-center bg-green-100 rounded-lg text-white hover:bg-green-200 transition ease-linear"
+      >
+        Nova solicitação
+      </a>
+    </div>
+  );
 }
