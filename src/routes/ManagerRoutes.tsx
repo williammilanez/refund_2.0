ff77154c1e013db4839a7cd65105b22ef8ad6c9c
William Milanez <william.milanez@outlook.com>
2026-04-04 17:42:43 -0300

    Criando a Página Dashboard. Componente: Criei o dashboard.tsx exportando uma função simples com um título. Configuração de Rotas: Criei o arquivo manager.routes para centralizar as rotas do dashboard. Fallback: Defini a rota principal e uma rota de "Página não encontrada" (404). Integração: Atualizei o index principal para importar e renderizar essas novas rotas.

---
+++ src/routes/ManagerRoutes.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router";
+
+import { AppLayout } from "../components/AppLayout";
+
+import { Dashboard } from "../pages/Dashboard";
+import { NotFound } from "../pages/NotFound";
+
+export function ManagerRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter } from "react-router";
 
-/* import { AuthRoutes } from "./AuthRoutes"; */
-import { EmployeeRoutes } from "./EmployeeRoutes";
+// import { AuthRoutes } from "./AuthRoutes";
+// import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <EmployeeRoutes />
+      <ManagerRoutes />
     </BrowserRouter>
   );
 }
