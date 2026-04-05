24ac150962a36366afddd3d04c321c135df93938
William Milanez <william.milanez@outlook.com>
2026-04-05 00:01:29 -0300

    feat(auth): implementar controle de acesso baseado em roles
    - Configura roteamento condicional conforme perfil do usuário
    - Utiliza switch case para definir rotas (user, manager)
    - Redireciona para login na ausência de sessão
    - Centraliza lógica de navegação
    - Melhora fluxo de autenticação e autorização
    
    Ref: Aula "Role do Usuário"

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,19 +3,37 @@ import { BrowserRouter } from "react-router";
 import { Loading } from "../components/Loading";
 
 import { AuthRoutes } from "./AuthRoutes";
-// import { EmployeeRoutes } from "./EmployeeRoutes";
-// import { ManagerRoutes } from "./ManagerRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 
 const isLoading = false;
+//const session = undefined;
+
+const session = {
+  user: {
+    role: "employee",
+  },
+};
 
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
   if (isLoading) {
     return <Loading />;
   }
 
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
