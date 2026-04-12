adb87afe1b74f35121ee3e11d12eb8a7d3a5f372
William Milanez <william.milanez@outlook.com>
2026-04-12 12:56:32 -0300

    feat: add form validation with zod for user registration

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,19 +1,52 @@
 import { useState } from "react";
 
+import { z, ZodError } from "zod";
+
 import { Button } from "../components/Button";
 import { Input } from "../components/input";
+
+const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Informe o nome " }),
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve ter pelo menos 6 dígitos" }),
+    passwordConfirm: z.string({ message: "Confirme a senha" }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais",
+    path: ["passwordConfirm"],
+  });
 
 export function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [isLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    console.log(name, email, password, passwordConfirm);
+    try {
+      setIsLoading(true);
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      alert("Não foi possível cadastrar!");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
