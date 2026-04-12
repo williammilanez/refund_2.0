242b8308bad7ffa8d0166c19b4cb3876dc19f6f5
William Milanez <william.milanez@outlook.com>
2026-04-12 16:20:56 -0300

    refactor: use useActionState for sign-in form state management

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,30 +1,39 @@
-import { useState } from "react";
+import { useActionState } from "react";
 
 import { Button } from "../components/Button";
 import { Input } from "../components/input";
 
 export function SignIn() {
-  const [isLoading] = useState(false);
+  const [state, formAction, isLoading] = useActionState(signIn, {
+    email: "",
+    password: "",
+  });
 
-  function onAction(formData: FormData) {
-    console.log(formData.get("password"));
+  async function signIn(prevState: any, formData: FormData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    return { email, password };
   }
 
   return (
-    <form action={onAction} className="w-full flex flex-col gap-4">
+    <form action={formAction} className="w-full flex flex-col gap-4">
       <Input
         name="email"
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        defaultValue={String(state?.email)}
       />
+
       <Input
         name="password"
         required
         legend="Senha"
         type="password"
         placeholder="123456"
+        defaultValue={String(state?.password)}
       />
 
       <Button type="submit" isLoading={isLoading}>
