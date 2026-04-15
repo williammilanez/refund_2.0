fc7e20bd1aa192e990eba965c76a93691bbeb09e
William Milanez <william.milanez@outlook.com>
2026-04-14 22:46:43 -0300

    feat: persist auth session using localStorage

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -5,12 +5,20 @@ type AuthContext = {
   save: (data: UserAPIResponse) => void;
 };
 
+const LOCAL_STORAGE_KEY = "@refund";
+
 export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null);
 
   function save(data: UserAPIResponse) {
+    localStorage.setItem(
+      `${LOCAL_STORAGE_KEY}:user`,
+      JSON.stringify(data.user),
+    );
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token);
+
     setSession(data);
   }
 
