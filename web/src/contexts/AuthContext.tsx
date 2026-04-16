d12cc99d6bd75f6bd00e5bfd3ae237900ff3729e
William Milanez <william.milanez@outlook.com>
2026-04-15 22:46:37 -0300

    feat: implement user logout and clear auth session

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ type AuthContext = {
   isLoading: boolean;
   session: null | UserAPIResponse;
   save: (data: UserAPIResponse) => void;
+  remove: () => void;
 };
 
 const LOCAL_STORAGE_KEY = "@refund";
@@ -22,6 +23,15 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token);
 
     setSession(data);
+  }
+
+  function remove() {
+    setSession(null);
+
+    localStorage.removeItem(`${LOCAL_STORAGE_KEY}:user`);
+    localStorage.removeItem(`${LOCAL_STORAGE_KEY}:token`);
+
+    window.location.assign("/");
   }
 
   function loadUser() {
@@ -43,7 +53,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ session, save, isLoading }}>
+    <AuthContext.Provider value={{ session, save, isLoading, remove }}>
       {children}
     </AuthContext.Provider>
   );
