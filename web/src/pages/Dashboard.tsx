5dfba69093b7010728084dcf464cf138006b1e98
William Milanez <william.milanez@outlook.com>
2026-04-18 14:55:07 -0300

    feat: fetch refund requests with filters and pagination

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+
+import { api } from "../services/api";
 
 import searchSvg from "../assets/search.svg";
-
 import { CATEGORIES } from "../utils/categories";
 import { formatCurrency } from "../utils/formatCurrency";
 
@@ -19,16 +21,30 @@ const REFUND_EXAMPLE = {
   categoryImg: CATEGORIES["transport"].icon,
 };
 
+const PER_PAGE = 5;
+
 export function Dashboard() {
   const [name, setName] = useState("");
   const [page, setPage] = useState(1);
-  const [totalOfPage] = useState(10);
-  const [refunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
+  const [totalOfPage, setTotalOfPage] = useState(0);
+  const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
 
-  function fetchRefunds(e: React.FormEvent) {
-    e.preventDefault();
+  async function fetchRefunds() {
+    try {
+      const response = await api.get(
+        `/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`,
+      );
 
-    console.log(name);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não foi possível carregar");
+    }
   }
 
   function handlePagination(action: "next" | "previous") {
@@ -44,6 +60,10 @@ export function Dashboard() {
       return prevPage;
     });
   }
+
+  useEffect(() => {
+    fetchRefunds();
+  }, []);
 
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
