e325b78de53c539081cffb4745f4dd66bf71ca10
William Milanez <william.milanez@outlook.com>
2026-04-19 01:14:30 -0300

    feat: handle search form submission without page reload

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -49,6 +49,11 @@ export function Dashboard() {
     }
   }
 
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    fetchRefunds();
+  }
+
   function handlePagination(action: "next" | "previous") {
     setPage((prevPage) => {
       if (action === "next" && prevPage < totalOfPage) {
@@ -72,7 +77,7 @@ export function Dashboard() {
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
       <form
-        onSubmit={fetchRefunds}
+        onSubmit={onSubmit}
         className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
       >
         <Input
