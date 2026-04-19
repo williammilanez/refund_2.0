0de24aaf3a9bd4794dc46cd1de6435513af1d684
William Milanez <william.milanez@outlook.com>
2026-04-19 01:09:26 -0300

    feat: display refund requests with pagination

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -13,21 +13,13 @@ import { Pagination } from "../components/Pagination";
 import type { RefundItemProps } from "../components/RefundItem";
 import { RefundItem } from "../components/RefundItem";
 
-const REFUND_EXAMPLE = {
-  id: "123",
-  name: "William",
-  category: "Transporte",
-  amount: formatCurrency(34.5),
-  categoryImg: CATEGORIES["transport"].icon,
-};
-
 const PER_PAGE = 5;
 
 export function Dashboard() {
   const [name, setName] = useState("");
   const [page, setPage] = useState(1);
   const [totalOfPage, setTotalOfPage] = useState(0);
-  const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
+  const [refunds, setRefunds] = useState<RefundItemProps[]>([]);
 
   async function fetchRefunds() {
     try {
@@ -35,7 +27,17 @@ export function Dashboard() {
         `/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`,
       );
 
-      console.log(response.data);
+      setRefunds(
+        response.data.refunds.map((refund) => ({
+          id: refund.id,
+          name: refund.user.name,
+          description: refund.name,
+          amount: formatCurrency(refund.amount),
+          categoryImg: CATEGORIES[refund.category].icon,
+        })),
+      );
+
+      setTotalOfPage(response.data.pagination.totalPages);
     } catch (error) {
       console.log(error);
 
